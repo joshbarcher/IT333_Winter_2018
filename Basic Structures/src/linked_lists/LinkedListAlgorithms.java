1161c28d2a036c9a783e58c83253d49806ea2c8e
Josh Archer <jarcher@greenriver.edu>
2018-01-24 22:07:55 -0800

    Added a few variations of the reverse algorithm for a singly linked list.

--- Basic Structures/src/linked_lists/LinkedListAlgorithms.java
+++ Basic Structures/src/linked_lists/LinkedListAlgorithms.java
@@ -1,5 +1,7 @@
 package linked_lists;
 
+import java.util.Stack;
+
 public class LinkedListAlgorithms
 {
     public static void main(String[] args)
@@ -9,7 +11,7 @@ public class LinkedListAlgorithms
 
         //assemble a linked list
         Node head = null;
-        for (int i = 1; i <= 500; i++)
+        for (int i = 1; i <= 5; i++)
         {
             if (head == null)
             {
@@ -21,8 +23,11 @@ public class LinkedListAlgorithms
             }
         }
 
-        //reverse it
-        head = algorithms.reverse(head);
+        //reverse the linked list using different techniques
+
+        //head = algorithms.reverse(head);
+        //head = algorithms.recursiveReverse(head);
+        head = algorithms.reverseWithStack(head);
 
         //verify our results
         Node current = head;
@@ -31,6 +36,65 @@ public class LinkedListAlgorithms
             //print the current element and move past it
             System.out.println(current.data);
             current = current.next;
+        }
+    }
+
+    public Node reverseWithStack(Node head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+
+        Stack<Node> stack = new Stack<>();
+
+        //add all nodes to the stack
+        while (head != null)
+        {
+            stack.push(head);
+            head = head.next;
+        }
+
+        //remove them one at a time
+        head = stack.pop();
+        Node current = head;
+
+        while (!stack.isEmpty())
+        {
+            Node next = stack.pop();
+            current.next = next;
+            current = next;
+        }
+        current.next = null;
+
+        return head;
+    }
+
+    public Node recursiveReverse(Node head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+        else
+        {
+            return recursiveReverse(head, null);
+        }
+    }
+
+    public Node recursiveReverse(Node current, Node newList)
+    {
+        if (current == null)
+        {
+            return newList;
+        }
+        else
+        {
+            Node savedNode = current.next;
+            current.next = newList;
+            newList = current;
+
+            return recursiveReverse(savedNode, newList);
         }
     }
 
