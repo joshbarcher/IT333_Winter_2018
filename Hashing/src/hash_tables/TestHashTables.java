cca74c480483fe1b8978a42c62c273ce88f33bf1
Josh Archer <jarcher@greenriver.edu>
2018-03-05 14:46:46 -0800

    Wrote a simple hash table based on linear probing.

---
+++ Hashing/src/hash_tables/TestHashTables.java
@@ -0,0 +1,32 @@
+package hash_tables;
+
+import java.util.Random;
+
+public class TestHashTables
+{
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+
+        HashTable<Integer> numberTable = new HashTable<>();
+
+        for (int i = 1; i <= 100; i++)
+        {
+            numberTable.add(random.nextInt(1000));
+        }
+
+        numberTable.printUsedSpaces();
+
+        for (int i = 0 ; i < 1000; i++)
+        {
+            System.out.println(i + ": " + numberTable.contains(i));
+        }
+
+        for (int i = 0; i < 500; i++)
+        {
+            numberTable.remove(i);
+        }
+
+        numberTable.printUsedSpaces();
+    }
+}
